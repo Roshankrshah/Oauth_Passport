cd6ef4ef55720cbabf6728d7405b045c2670760c
Roshan <shahroshan0720@gmail.com>
2023-08-11 22:27:49 +0530

    page setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+const port = 2002;
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
