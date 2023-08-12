cfc10a04a762573bea08a78e808a132b17653afe
Roshan <shahroshan0720@gmail.com>
2023-08-12 21:20:33 +0530

    profile view setup

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -5,14 +5,12 @@ const authCheck = (req,res,next)=>{
     if(!req.user){
         res.redirect('/auth/login');
     }else{
-        console.log('bye');
         next();
     }
 };
 
 router.get('/',authCheck,(req,res)=>{
-    console.log('hi');
-    res.send('suck my dick '+req.user.username);
+    res.render('profile', { user: req.user });
 });
 
 module.exports = router;
